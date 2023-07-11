7a6c2d2415bd458993cbd61996d877b27c88185a
sshamshiri <sarashamshiri1398@gmail.com>
2023-07-11 14:16:24 +0330

    migrate Task model

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE ,blank=True , null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True , null=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
