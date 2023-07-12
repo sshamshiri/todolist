de657b36f2fc65994674100ddbb23bcbb44b7e89
sshamshiri <sarashamshiri1398@gmail.com>
2023-07-12 10:38:26 +0330

    add classes TaskCreate,TaskUpdate,TaskDelete

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -5,5 +5,8 @@ app_name = 'core'
 urlpatterns = [
     path('', views.TaskList.as_view() , name='tasks'),
     path('task/<int:pk>', views.TaskDetail.as_view() , name='task'),
+    path('task-create/', views.TaskCreate.as_view() , name='task-create'),
+    path('task-update/<int:pk>', views.TaskUpdate.as_view() , name='task-update'),
+    path('task-delete/<int:pk>', views.TaskDelete.as_view() , name='task-delete'),
 
 ]
--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Task
+from django.urls import reverse_lazy
 
 class TaskList(generic.ListView):
     model = Task
@@ -11,3 +12,18 @@ class TaskDetail(generic.DetailView):
     model = Task
     template_name = 'core/task_detail.html'
     context_object_name = 'task'
+
+class TaskCreate(generic.CreateView):
+    model = Task
+    fields = '__all__'
+    success_url =reverse_lazy('core:tasks')
+    
+class TaskUpdate(generic.UpdateView):
+    model =Task
+    fields = '__all__'
+    success_url =reverse_lazy('core:tasks')
+
+class TaskDelete(generic.DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url =reverse_lazy('core:tasks')
